c6a0ac1c88dc540a674bda692de1c770af01dde8
branyerbjr <branyer.romero@tecsup.edu.pe>
2023-05-25 21:10:25 -0500

    db conexion

--- components/Conexion.php
+++ components/Conexion.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
+$hostdb = "cli3wx8z80079s6ah7bpie476"; 
 $password = "SZOtrdgNresfIBLi2BK8PcaN";
 $usuario = "cli3wx8z60032ahs6dbvs5ac8";
 $nombre_bd = "chatrapido";
 
 try {
 	$bd = new PDO (
-		'mysql:host=cli3wx8z80079s6ah7bpie476:9001;
-		dbname='.$nombre_bd,
+		'mysql:host='.$hostdb
+		.'dbname='.$nombre_bd,
 		$usuario,
 		$password,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
