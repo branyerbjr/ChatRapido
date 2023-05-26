05a97d95be544ec7a0ff49ba64ae12dd3726b08e
branyerbjr <branyer.romero@tecsup.edu.pe>
2023-05-25 21:28:43 -0500

    correccion database

--- components/Conexion.php
+++ components/Conexion.php
@@ -1,13 +1,12 @@
-<?php
-$hostdb = "cli3wx8z80079s6ah7bpie476:9001"; 
+<?php 
 $password = "SZOtrdgNresfIBLi2BK8PcaN";
 $usuario = "cli3wx8z60032ahs6dbvs5ac8";
 $nombre_bd = "chatrapido";
 
 try {
 	$bd = new PDO (
-		'mysql:host='.$hostdb
-		.'dbname='.$nombre_bd,
+		'mysql:host=cli3wx8z80079s6ah7bpie476:3306;
+		dbname='.$nombre_bd,
 		$usuario,
 		$password,
 		array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
