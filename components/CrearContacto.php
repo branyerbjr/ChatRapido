0ba474740bbccc4feb8ebcbeb576a9e63b9ab71a
branyerbjr <branyer.romero@tecsup.edu.pe>
2023-05-31 09:39:31 -0500

    cambiando el sql

--- components/CrearContacto.php
+++ components/CrearContacto.php
@@ -10,7 +10,7 @@ $ap_materno = $_POST["txtApMaterno"];
 $fecha_nacimiento = $_POST["txtFechaNacimiento"];
 $celular = $_POST["txtCelular"];
 
-$sentencia = $bd->prepare("INSERT INTO contacto(nombres,apellido_paterno,apellido_materno,fecha_nacimiento,celular) VALUES (?,?,?,?,?);");
+$sentencia = $bd->prepare("INSERT INTO chatrapido.contacto(nombres,apellido_paterno,apellido_materno,fecha_nacimiento,celular) VALUES (?,?,?,?,?);");
 $resultado = $sentencia->execute([$nombres, $ap_paterno, $ap_materno, $fecha_nacimiento, $celular]);
 
 if ($resultado === TRUE) {
